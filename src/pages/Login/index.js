74253faa44c81cec5cb723348b5ca678bec90448
Rafael <rafa.comp@gmail.com>
2019-06-25 10:53:55 -0300

    implementacao do state

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -12,19 +12,38 @@ import {
 } from './styles';
 
 import Pizza from '~/assets/images/logo.png';
+import Background from '~/assets/images/background.png';
 
 export default class Login extends Component {
   componentDidMount() {}
 
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleLogin = () => {};
+
   render() {
     const { navigation } = this.props;
     return (
-      <BackgroundImage source={require('../../assets/images/background.png')} resizeMode="cover">
+      <BackgroundImage source={Background} resizeMode="cover">
         <LinearGradient locations={[0.1, 1]} colors={['transparent', '#000']} style={{ flex: 1 }}>
           <Container>
             <Logo source={Pizza} />
-            <Input placeholder="Seu e-mail" />
-            <Input placeholder="Senha secreta" />
+            <Input
+              placeholder="Seu e-mail"
+              autoCapitalize="none"
+              value={this.state.email}
+              onChangeText={text => this.setState({ email: text })}
+            />
+            <Input
+              placeholder="Senha secreta"
+              secureTextEntry
+              autoCapitalize="none"
+              value={this.state.password}
+              onChangeText={text => this.setState({ password: text })}
+            />
             <Button onPress={() => navigation.navigate('App')}>
               <TextButton>Entrar</TextButton>
             </Button>
