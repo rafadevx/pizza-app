3d84fe4f8d7e57d0d2835bb348df229653d76563
Rafael <rafa.comp@gmail.com>
2019-06-25 10:54:10 -0300

    inclusao e configuracao do axios

---
+++ src/services/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { AsyncStorage } from 'react-native';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3030',
+});
+
+api.interceptors.request.use(async (config) => {
+  try {
+    const token = await AsyncStorage.getItem('@pizza:token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  } catch (err) {
+    console.tron.log(err);
+  }
+  return config;
+});
+
+export default api;
